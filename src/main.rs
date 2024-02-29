d93c50e7491344a0b80292c4f65aed3fb6e3a062
jihed krouna <jihedkrouna@gmail.com>
2024-02-29 16:44:35 -0500

    progress

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,30 @@
 #![allow(unused_imports)]
 #![allow(unused_mut)]
 
-use std::fmt::format;
+use std::{fmt::format, u16::MAX};
 fn main() {
     println!("Hello, world!");
 }
 //49. Group Anagrams first medium problem
-pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {}
+pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+    let mut result: Vec<Vec<String>> = Vec::new();
+    for string in strs {
+        if result.is_empty() {
+            let mut temp_vec: Vec<String> = Vec::new();
+            temp_vec.push(string);
+            result.push(temp_vec);
+        } else {
+            let mut group_index: u16 = 0;
+            let mut string_index: u8 = 0;
+            for group in &result {
+                for string in group {}
+            }
+        }
+    }
+    return vec![vec!["s".to_string()]];
+}
 #[cfg(test)]
 mod tests {
-
-    use std::fmt::Binary;
 
     use super::*;
 
