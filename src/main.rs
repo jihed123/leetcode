52ce22e4898a8511ddcd4192399280ac56887f16
jihed krouna <jihedkrouna@gmail.com>
2024-02-29 17:24:55 -0500

    progress

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,43 @@ fn main() {
 //49. Group Anagrams first medium problem
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
     let mut result: Vec<Vec<String>> = Vec::new();
+
     for string in strs {
         if result.is_empty() {
             let mut temp_vec: Vec<String> = Vec::new();
             temp_vec.push(string);
             result.push(temp_vec);
         } else {
-            let mut group_index: u16 = 0;
-            let mut string_index: u8 = 0;
-            for group in &result {
-                for string in group {}
+            let mut group_index: i16 = -1;
+            // fix the cloning cost
+            for group in result.clone() {
+                group_index += 1;
+                // maybe i can find a way to cache this (in another array to win time for redendences)
+                let mut characters: Vec<char> = group[0].chars().collect();
+                characters.sort_unstable();
+                let mut string_sorted: Vec<char> = string.clone().chars().collect();
+                string_sorted.sort_unstable();
+                let mut string_index: i8 = -1;
+                let mut found_string = true;
+                for char in characters {
+                    string_index += 1;
+                    if char != string_sorted[string_index as usize] {
+                        found_string = false;
+                        break;
+                    }
+                }
+                if found_string {
+                    result[group_index as usize].push(string.clone());
+                } else {
+                    let mut temp_vec: Vec<String> = Vec::new();
+                    temp_vec.push(string.clone());
+                    result.push(temp_vec);
+                }
             }
         }
     }
-    return vec![vec!["s".to_string()]];
+
+    return result;
 }
 #[cfg(test)]
 mod tests {
